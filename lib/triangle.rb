511dc084ca1a96951f854481d2e02609f092fd8a
Sarah Larkworthy <sarah.larkworthy@gmail.com>
2020-02-19 20:50:00 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,6 @@ class Triangle
     if (@side1 == 0) || (@side2 == 0) || (@side3 == 0)
       begin
         raise TriangleError
-      rescue TriangleError
       end
     elsif (@side1 == @side2) && (@side2 == @side3)
       :equilateral
