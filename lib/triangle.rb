8c94d47a800c93cd165ba321cf945221e66777b9
Sarah Larkworthy <sarah.larkworthy@gmail.com>
2020-02-19 20:38:26 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,11 @@ class Triangle
   def kind 
     if (@side1 == @side2) && (@side2 == @side3)
       :equilateral
-    
+    if (@side1 == @side2) || (@side2 == @side3) || (@side1 == @side3)
+      :isosceles
+    if !(@side1 == @side2) && !(@side2 == @side3) && !(@side1 == @side3)
+      :scalene
+    end
     
   end
   
