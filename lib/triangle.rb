7bad71b8a7e01ab275ef5b11b8d61d40ede829d3
Sarah Larkworthy <sarah.larkworthy@gmail.com>
2020-02-19 20:52:06 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,8 +8,9 @@ class Triangle
   end
   
   def kind 
-    if (@side1 == 0) || (@side2 == 0) || (@side3 == 0)
+    if (@side1 <= 0) || (@side2 <= 0) || (@side3 <= 0)
         raise TriangleError
+        
     elsif (@side1 == @side2) && (@side2 == @side3)
       :equilateral
     elsif (@side1 == @side2) || (@side2 == @side3) || (@side1 == @side3)
@@ -20,9 +21,6 @@ class Triangle
   end
   
   class TriangleError < StandardError
-    def message
-      puts "This is an illegal triangle."  
-    end
   end
   
 end
